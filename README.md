2e5afd76c54d06159eea3107453af6cfed1100de
Dennis Dietrich <d.dietrich84@gmail.com>
2021-02-01 19:30:36 +0100

    ...

---
+++ README.md
@@ -0,0 +1,22 @@
+WIP
+
+## Install
+
+```bash
+npm i
+npm link
+cd ~/my-project
+npm link codemirror-themes
+```
+
+## Usage
+
+```javascript
+import {materialLight} from 'codemirror-themes/theme/material-light'
+import {materialDark} from 'codemirror-themes/theme/material-dark'
+import {solarizedLight} from 'codemirror-themes/theme/solarized-light'
+import {solarizedDark} from 'codemirror-themes/theme/solarized-dark'
+import {dracula} from 'codemirror-themes/theme/dracula'
+import {githubLight} from 'codemirror-themes/theme/github-light'
+import {githubDark} from 'codemirror-themes/theme/github-dark'
+```
--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,27 @@
 import typescript from '@rollup/plugin-typescript'
+import {readdirSync} from 'fs'
+import {join} from 'path'
 
-export default {
+const themes = join(__dirname, 'theme')
+
+const config = readdirSync(themes).map((f) => ({
+  input: join(themes, f),
+  output: [{
+    format: 'esm',
+    dir: './dist/theme',
+    externalLiveBindings: false,
+  }],
+  plugins: [typescript()],
+  external: id => id != 'tslib' && !/^(\.?\/|\w:)/.test(id),
+}))
+
+export default [{
   input: './index.ts',
   output: [{
     format: 'esm',
     dir: "./dist",
     externalLiveBindings: false,
-  }, {
-    format: 'cjs',
-    file: './dist/index.cjs',
   }],
   plugins: [typescript()],
-  external: id => id != "tslib" && !/^(\.?\/|\w:)/.test(id),
-}
+  external: id => id != 'tslib' && !/^(\.?\/|\w:)/.test(id),
+}, ...config]
