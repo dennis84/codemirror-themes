52b2fcab564d1a623db8b8a6bc2981709f69cb72
Dennis Dietrich <d.dietrich84@gmail.com>
2021-08-28 12:16:34 +0200

    revert

--- theme/dracula.ts
+++ theme/dracula.ts
@@ -50,8 +50,8 @@ export const draculaTheme = EditorView.theme({
     backgroundColor: config.selection
   },
 
-  '&.cm-focused .cm-activeLine': {backgroundColor: config.selection},
-  '&.cm-focused .cm-activeLineGutter': {backgroundColor: config.background},
+  '.cm-activeLine': {backgroundColor: config.selection},
+  '.cm-activeLineGutter': {backgroundColor: config.background},
   '.cm-selectionMatch': {backgroundColor: config.selection},
 
   '.cm-matchingBracket, .cm-nonmatchingBracket': {
