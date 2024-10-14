2434a58bc913cb2e58e107eced0d6eac5b750e2f
Dennis Dietrich <d.dietrich84@gmail.com>
2024-10-14 23:14:03 +0200

    export cjs files

--- rollup.config.js
+++ rollup.config.js
@@ -6,25 +6,41 @@ const themes = './theme'
 
 const builds = readdirSync(themes).map((f) => ({
   input: join(themes, f),
-  output: [{
-    format: 'esm',
-    dir: './dist/theme',
-    externalLiveBindings: false,
-  }],
+  output: [
+    {
+      format: 'esm',
+      dir: './dist/theme',
+      externalLiveBindings: false,
+    },
+    {
+      format: 'cjs',
+      dir: './dist/theme',
+      externalLiveBindings: false,
+      entryFileNames: '[name].cjs',
+    },
+  ],
   plugins: [typescript()],
-  external: id => id != 'tslib' && !/^(\.?\/|\w:)/.test(id),
+  external: (id) => id != 'tslib' && !/^(\.?\/|\w:)/.test(id),
 }))
 
 export default [
   ...builds,
   {
     input: './index.ts',
-    output: [{
-      format: 'esm',
-      dir: './dist',
-      externalLiveBindings: false,
-    }],
+    output: [
+      {
+        format: 'esm',
+        dir: './dist',
+        externalLiveBindings: false,
+      },
+      {
+        format: 'cjs',
+        dir: './dist',
+        externalLiveBindings: false,
+        entryFileNames: '[name].cjs',
+      },
+    ],
     plugins: [typescript()],
-    external: id => id != 'tslib' && !/^(\.?\/|\w:)/.test(id),
-  }
+    external: (id) => id != 'tslib' && !/^(\.?\/|\w:)/.test(id),
+  },
 ]
